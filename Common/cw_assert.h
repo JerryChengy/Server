de8992ce0c4c96bf7e17d42d91a339e89ffebb13
jerrychengy <chengywnn@aliyun.com>
2016-07-12 14:45:42 +0800

    assert fix bug

--- Common/cw_assert.h
+++ Common/cw_assert.h
@@ -6,7 +6,7 @@ void AssertShowEx(char* file, int line, char* func, char* expr, char* format, ..
 void AssertShow(char* file, int line, char* func, char* expr);
 
 
-#define Assert(expr) {if(!expr) AssertShow(__FILE__, __LINE__, __FUNCTION__, #expr);}
+#define Assert(expr) {if(!(expr)) AssertShow(__FILE__, __LINE__, __FUNCTION__, #expr);}
 #define AssertEx(expr, szFormat, ...) {if (!(expr)) AssertShowEx(__FILE__, __LINE__, __FUNCTION__, #expr, szFormat, __VA_ARGS__);}
 
 #endif
